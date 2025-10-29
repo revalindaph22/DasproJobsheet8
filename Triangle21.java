713e8cdaf5e4cb9f4f82cd4d71c6dd19c3adf8c1
revalinda <revalindaph@gmail.com>
2025-10-29 15:05:23 +0700

    Percobaan 2

---
+++ Triangle21.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Triangle21 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai N = ");
+        int N = sc.nextInt();
+        int i = 0;
+        while (i <= N) {
+            int j = 0;
+            while (j<i) {
+                System.out.println("*");
+                j++;
+            }
+            i++;
+        }
+    sc.close();
+    }
+}
